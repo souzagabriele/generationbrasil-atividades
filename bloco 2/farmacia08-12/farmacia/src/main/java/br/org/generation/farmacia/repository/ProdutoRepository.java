3fb4b4ac1c78ae890ec9b00ae4a4b300f370c013
Gabriele Souza <gabrielepsouzaa@gmail.com>
2021-12-08 20:28:47 -0300

    Adicionado Exercicio Farmacia

---
+++ bloco 2/farmacia08-12/farmacia/src/main/java/br/org/generation/farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,19 @@
+package br.org.generation.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.farmacia.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List <Produto> findAllByNomeContainingIgnoreCase(String nome);
+	 
+	public List <Produto> findByNomeAndLaboratorio(String nome, String laboratorio);
+	
+	public List <Produto> findByNomeOrLaboratorio(String nome, String laboratorio);
+
+}
